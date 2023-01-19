559a904eb817ffa475289c6bc3c23a241471a9c1
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-01-19 18:14:52 -0300

    feat: add days dynamic array field to simulation form

--- src/components/SimulationForm/index.tsx
+++ src/components/SimulationForm/index.tsx
@@ -1,9 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 
-import { useState } from "react";
 import { useAnticipationContext } from "../../providers";
 import { ISimulationRequest } from "../../providers/anticipationContext/interface";
 import { simulationSchema } from "../../validators";
@@ -12,6 +11,8 @@ import { Input } from "../Form/Input";
 const SimulationForm = (): JSX.Element => {
   const {
     register,
+    control,
+    getValues,
     handleSubmit,
     formState: { errors },
   } = useForm<ISimulationRequest>({
@@ -19,17 +20,34 @@ const SimulationForm = (): JSX.Element => {
     shouldFocusError: false,
     resolver: yupResolver(simulationSchema),
   });
+  const { fields, append, remove } = useFieldArray({
+    name: "days" as never,
+    control: control,
+  });
 
   const [simulation, setSimulation] = useState<ISimulationRequest | null>(null);
   const updateSimulation = handleSubmit(
     (data: ISimulationRequest) => {
+      console.log("validation: ", data);
       setSimulation({ ...data });
     },
     (errors) => console.log(errors)
   );
 
+  const removeDays = (index: number) => {
+    remove(index);
+
+    let days = getValues("days");
+    delete days?.[index];
+    if (!days?.length) {
+      days = undefined;
+    }
+    setSimulation({ ...simulation!, days });
+  };
+
   const { simulateAnticipation } = useAnticipationContext();
   useEffect(() => {
+    console.log("simulation: ", simulation);
     const timeoutSimulationID = setTimeout(() => {
       if (simulation) simulateAnticipation(simulation);
     }, 700);
@@ -46,22 +64,51 @@ const SimulationForm = (): JSX.Element => {
         <Input
           label="Informe o valor da venda"
           type="currency"
+          id="amount"
           {...register("amount")}
           defaultValue="R$ 0,00"
-          error={errors.amount?.message}
+          error={errors.amount?.message as string}
         />
         <Input
           label="Em quantas parcelas"
           type="number"
+          id="installments"
           {...register("installments", { valueAsNumber: true })}
-          error={errors.installments?.message}
+          error={errors.installments?.message as string}
         />
         <Input
           label="Informe o percentual de MDR"
           type="number"
+          id="mdr"
           {...register("mdr", { valueAsNumber: true })}
-          error={errors.mdr?.message}
+          error={errors.mdr?.message as string}
         />
+        <div>
+          <div>
+            <label>Período de tempo</label>
+            <span>Padrão de 1, 15, 30 e 90 dias</span>
+            <button
+              type="button"
+              onClick={() => {
+                append("");
+              }}
+            >
+              Append
+            </button>
+          </div>
+          {fields.map(({ id }, index) => (
+            <div key={id}>
+              <Input
+                type="number"
+                {...register(`days.${index}` as const, { valueAsNumber: true })}
+                error={errors.days?.[index]?.message}
+              />
+              <button type="button" onClick={() => removeDays(index)}>
+                Remover
+              </button>
+            </div>
+          ))}
+        </div>
       </form>
     </section>
   );
