ba9313ec2b280baabce41f705859634b2d9cf79e
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-01-22 16:16:32 -0300

    fix: change simulation form to use form context and add array field days

--- src/components/SimulationForm/index.tsx
+++ src/components/SimulationForm/index.tsx
@@ -1,49 +1,25 @@
 import { useEffect, useState } from "react";
 
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useFieldArray, useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 
 import { useAnticipationContext } from "../../providers";
 import { ISimulationRequest } from "../../providers/anticipationContext/interface";
 import { simulationSchema } from "../../validators";
-import { Input } from "../Form/Input";
+import { Input, InputArray, inputCurrencyChange } from "../FormFields";
 
 const SimulationForm = (): JSX.Element => {
-  const {
-    register,
-    control,
-    getValues,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ISimulationRequest>({
+  const methods = useForm<ISimulationRequest>({
     mode: "onChange",
     shouldFocusError: false,
     resolver: yupResolver(simulationSchema),
   });
-  const { fields, append, remove } = useFieldArray({
-    name: "days" as never,
-    control: control,
-  });
+  const { handleSubmit } = methods;
 
   const [simulation, setSimulation] = useState<ISimulationRequest | null>(null);
-  const updateSimulation = handleSubmit(
-    (data: ISimulationRequest) => {
-      console.log("validation: ", data);
-      setSimulation({ ...data });
-    },
-    (errors) => console.log(errors)
-  );
-
-  const removeDays = (index: number) => {
-    remove(index);
-
-    let days = getValues("days");
-    delete days?.[index];
-    if (!days?.length) {
-      days = undefined;
-    }
-    setSimulation({ ...simulation!, days });
-  };
+  const updateSimulation = handleSubmit((data: ISimulationRequest) => {
+    setSimulation({ ...data });
+  });
 
   const { simulateAnticipation } = useAnticipationContext();
   useEffect(() => {
@@ -60,56 +36,28 @@ const SimulationForm = (): JSX.Element => {
   return (
     <section>
       <h1>Simule Sua Antecipação</h1>
-      <form onChange={updateSimulation}>
-        <Input
-          label="Informe o valor da venda"
-          type="currency"
-          id="amount"
-          {...register("amount")}
-          defaultValue="R$ 0,00"
-          error={errors.amount?.message as string}
-        />
-        <Input
-          label="Em quantas parcelas"
-          type="number"
-          id="installments"
-          {...register("installments", { valueAsNumber: true })}
-          error={errors.installments?.message as string}
-        />
-        <Input
-          label="Informe o percentual de MDR"
-          type="number"
-          id="mdr"
-          {...register("mdr", { valueAsNumber: true })}
-          error={errors.mdr?.message as string}
-        />
-        <div>
-          <div>
-            <label>Período de tempo</label>
-            <span>Padrão de 1, 15, 30 e 90 dias</span>
-            <button
-              type="button"
-              onClick={() => {
-                append("");
-              }}
-            >
-              Append
-            </button>
-          </div>
-          {fields.map(({ id }, index) => (
-            <div key={id}>
-              <Input
-                type="number"
-                {...register(`days.${index}` as const, { valueAsNumber: true })}
-                error={errors.days?.[index]?.message}
-              />
-              <button type="button" onClick={() => removeDays(index)}>
-                Remover
-              </button>
-            </div>
-          ))}
-        </div>
-      </form>
+      <FormProvider {...methods}>
+        <form onChange={updateSimulation}>
+          <Input
+            label="Informe o valor da venda"
+            name="amount"
+            onChange={inputCurrencyChange}
+            defaultValue="R$ 0,00"
+          />
+          <Input
+            label="Em quantas parcelas"
+            type="number"
+            name="installments"
+          />
+          <Input label="Informe o percentual de MDR" type="number" name="mdr" />
+          <InputArray
+            label="Período de tempo"
+            subtitle="Padrão de 1, 15, 30 e 90 dias"
+            name="days"
+            onRemove={() => updateSimulation()}
+          />
+        </form>
+      </FormProvider>
     </section>
   );
 };
